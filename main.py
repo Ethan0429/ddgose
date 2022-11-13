ba8ee506255705b4869d2de3c8dbbd1c4330782a
Ethan Rickert <ethanrickert0429@gmail.com>
2022-11-12 19:14:09 -0500

    main.py inclusion

---
+++ main.py
@@ -0,0 +1,22 @@
+import constants as c
+import discord
+from discord.ext import commands
+
+
+def start():
+
+    # create bot with full intents
+    bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
+
+    # startup message
+    @bot.event
+    async def on_ready():
+        print('____________________________________________________________')
+        print('Ready to stonk!')
+
+    # run bot
+    bot.run(c.BOT_TOKEN)
+
+
+if __name__ == "__main__":
+    start()
